ad0ef370da92f2291aeb5818476a240ee9614d87
gwoo <gwoohoo@gmail.com>
2013-11-09 12:54:18 -0800

    Adding images.

--- images.go
+++ images.go
@@ -1,33 +1,71 @@
-// GCloud - Go Packages for Cloud Services.
+// GCloud - Go Packages for Cloud Servicei.
 // Copyright (c) 2013 Garrett Woodworth (https://github.com/gwoo).
 
 package compute
 
 import (
+	"encoding/json"
+	p "github.com/gcloud/compute/providers"
 	"github.com/gcloud/identity"
 )
 
 // The images available from the compute service.
 type Images struct {
-	identity.Account
+	Account  identity.Account
+	Provider string
 }
 
-type Image struct {
-	Id   string
-	Name string
-}
+// List images available on the account.
+func (i *Images) List() (*[]p.Image, error) {
+	provider := p.GetProvider(i.Provider, i.Account)
+	result, err := provider.Images.List()
+	if err != nil {
+		return nil, err
+	}
+	var records []p.Image
+	err = json.Unmarshal(result, &records)
 
-// List images available to the account.
-func (s *Images) List() {}
+	if err != nil {
+		return nil, err
+	}
+	return &records, err
+}
 
 // Show image information for a given id.
-func (s *Images) Show(id string) {}
+func (i *Images) Show(id string) (*p.Image, error) {
+	provider := p.GetProvider(i.Provider, i.Account)
+	result, err := provider.Images.Show(id)
+	if err != nil {
+		return nil, err
+	}
+	var record p.Image
+	err = json.Unmarshal(result, &record)
 
-// Create an image.
-func (s *Images) Create() {}
+	if err != nil {
+		return nil, err
+	}
+	return &record, err
+}
 
-// Destroy an image.
-func (s *Images) Destroy() {}
+// Create a image.
+func (i *Images) Create(n *p.Image) (*p.Image, error) {
+	provider := p.GetProvider(i.Provider, i.Account)
+	result, err := provider.Images.Create(n)
+	if err != nil {
+		return nil, err
+	}
+	var record p.Image
+	err = json.Unmarshal(result, &record)
 
-// Distribute images to mutliple regions.
-func (s *Images) Distribute() {}
+	if err != nil {
+		return nil, err
+	}
+	return &record, err
+}
+
+// Destroy a image.
+func (i *Images) Destroy(id string) (bool, error) {
+	provider := p.GetProvider(i.Provider, i.Account)
+	ok, err := provider.Images.Destroy(id)
+	return ok, err
+}
--- images_test.go
+++ images_test.go
@@ -4,23 +4,95 @@
 package compute
 
 import (
+	p "github.com/gcloud/compute/providers"
 	"testing"
 )
 
+type MockImages struct{}
+
+// List images available on the account.
+func (s *MockImages) List() ([]byte, error) {
+	return []byte(`[{"Name":"My Image","Id": "616fb98f-46ca-475e-917e-2563e5a8cd19"}]`), nil
+}
+func (s *MockImages) Show(id string) ([]byte, error) {
+	return []byte(`{"Name":"My Image","Id": "616fb98f-46ca-475e-917e-2563e5a8cd19"}`), nil
+}
+func (s *MockImages) Create(n *p.Image) ([]byte, error) {
+	return []byte(`{"Name":"My Image","Id": "616fb98f-46ca-475e-917e-2563e5a8cd19"}`), nil
+}
+func (s *MockImages) Destroy(id string) (bool, error) {
+	return true, nil
+}
+
+func init() {
+	p.RegisterImages("mock", &MockImages{})
+}
+
 func Test_ImagesList(t *testing.T) {
-	t.Error("ImagesList failed.")
+	images := &Images{Provider: "mock"}
+	results, err := images.List()
+	if err != nil {
+		t.Error("Images List failed with " + err.Error() + "(bool, error).")
+	}
+	if results == nil {
+		t.Error("Results should not be nil.")
+	}
+	for _, v := range *results {
+		if v.Id != "616fb98f-46ca-475e-917e-2563e5a8cd19" {
+			t.Error("Wrong value for Id.")
+		}
+		if v.Name != "My Image" {
+			t.Error("Wrong value for Name.")
+		}
+	}
 }
 
 func Test_ImagesShow(t *testing.T) {
-	t.Error("ImagesShow failed.")
+	images := &Images{Provider: "mock"}
+	result, err := images.Show("616fb98f-46ca-475e-917e-2563e5a8cd19")
+	if err != nil {
+		t.Error("Images Show failed with " + err.Error() + ".")
+	}
+	if result == nil {
+		t.Error("Results should not be nil.")
+	}
+	r := *result
+	if r.Id != "616fb98f-46ca-475e-917e-2563e5a8cd19" {
+		t.Error("Wrong value for Id.")
+	}
+	if r.Name != "My Image" {
+		t.Error("Wrong value for Name.")
+	}
 }
 
 func Test_ImagesCreate(t *testing.T) {
-	t.Error("ImagesCreate failed.")
+	images := &Images{Provider: "mock"}
+	result, err := images.Create(&p.Image{
+		Name: "My Image"})
+	if err != nil {
+		t.Error("Images Create failed with " + err.Error() + ".")
+	}
+	if result == nil {
+		t.Error("Results should not be nil.")
+	}
+	r := *result
+	if r.Id != "616fb98f-46ca-475e-917e-2563e5a8cd19" {
+		t.Error("Wrong value for Id.")
+	}
+	if r.Name != "My Image" {
+		t.Error("Wrong value for Name.")
+	}
 }
 
 func Test_ImagesDestroy(t *testing.T) {
-	t.Error("ImagesDestroy failed.")
+	images := &Images{Provider: "mock"}
+	ok, err := images.Destroy("616fb98f-46ca-475e-917e-2563e5a8cd19")
+	if !ok {
+		t.Error("Images Destroy failed.")
+	}
+	if err != nil {
+		t.Error("Images Destroy failed with " + err.Error() + ".")
+	}
 }
 
 func Test_ImagesDistribute(t *testing.T) {
